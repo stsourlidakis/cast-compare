265ab7247737adfae1bf5e3c9fbae96c43ab03f0
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-06 00:10:53 +0300

    add a placeholder image when the poster is missing

--- src/components/Person/Credits/Credit/Credit.js
+++ src/components/Person/Credits/Credit/Credit.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import missing from '../../../../assets/images/missingPoster.png';
+
 const Credit = (props) => {
 	const title = props.data.media_type==='tv' ? props.data.name : props.data.title;
 	const released = props.data.release_date ? `(${props.data.release_date})` : '';
+	const imgSrc = props.data.poster_path ? `https://image.tmdb.org/t/p/w92/${props.data.poster_path}` : missing;
 	return (
-			<a
-				href={`https://www.themoviedb.org/movie/${props.data.id}`}
-				target="_blank"
-				rel="noopener noreferrer">
-					<img
-						src={`https://image.tmdb.org/t/p/w92/${props.data.poster_path}`}
-						alt={title}
-						title={`${title} ${released}`} />
-			</a>
+		<a
+			href={`https://www.themoviedb.org/movie/${props.data.id}`}
+			target="_blank"
+			rel="noopener noreferrer">
+				<img
+					src={imgSrc}
+					alt={title}
+					title={`${title} ${released}`} />
+		</a>
 	);
 };
 
