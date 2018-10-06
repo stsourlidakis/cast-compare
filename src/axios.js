855e1be533f7c83f0e8a8e065bd38b4b067109da
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-06 20:00:51 +0300

    add a production autocomplete url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const personAutocomplete = axios.create({
-	baseURL: 'http://localhost:3001'
+	baseURL: process.env.NODE_ENV==='production' ? process.env.REACT_APP_AUTOCOMPLETE_URL : 'http://localhost:3001'
 });
 
 const theMovieDB = axios.create({
