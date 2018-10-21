39096077d740423c2efb84ad230c1d90b3532075
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-22 01:02:09 +0300

    disable the active page's link

--- src/components/UI/Menu/Menu.js
+++ src/components/UI/Menu/Menu.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import styles from './Menu.module.css';
  
 const Menu = (props) => {
+	const pages = ['movies', 'people'];
+	const defaultPage = 'movies';
+	let activePage = props.location.pathname.split('/')[1];
+	if(activePage===''){	//in case of /
+		activePage=defaultPage;
+	}
+
 	return (
 		<div className={styles.Menu}>
-			<Link to="/movies" className={styles.link}>Compare Movies</Link>
-			<Link to="/people" className={styles.link}>Compare People</Link>
+			{pages.map(page =>
+				<Link
+					to={`/${page}`}
+					className={[styles.link, page===activePage ? styles.active : null].join(' ')}
+					key={page}>{`Compare ${page}`}</Link>
+			)}
 		</div>
 	);
 };
 
-export default Menu;
+export default withRouter(Menu);
