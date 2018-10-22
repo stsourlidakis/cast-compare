ae901e5472e7e9a35dd7f68642b30dad34ae4418
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-10-22 21:13:06 +0300

    add the project's repo link

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
 	return (
 		<div className={styles.Footer}>
 			{/*eslint-disable-next-line*/}
-			Developed by <a href="https://stsourlidakis.com" target="_blank">stsourlidakis</a> with data from the <a href="https://www.themoviedb.org/documentation/api" target="_blank" rel="noopener noreferrer" title="This product uses the TMDb API but is not endorsed or certified by TMDb.">TMDb API</a>.
+			<a href="https://github.com/stsourlidakis/cast-compare" target="_blank">Developed</a> by <a href="https://stsourlidakis.com" target="_blank">stsourlidakis</a> with data from the <a href="https://www.themoviedb.org/documentation/api" target="_blank" rel="noopener noreferrer" title="This product uses the TMDb API but is not endorsed or certified by TMDb.">TMDb API</a>.
 		</div>
 	);
 };
